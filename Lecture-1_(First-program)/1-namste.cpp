44a4deda4acda171bf272e700328ad2690cc2c28
Sachinkr <royalsachin1530@gmail.com>
2025-07-17 01:11:17 +0530

    Add files via upload

---
+++ Lecture-1_(First-program)/1-namste.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   cout<<"namste bharat"<<endl;
+   cout<<'2';
+}
---
+++ Lecture-1_(First-program)/2-input.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // cout<<"enter any number "<<endl;
+    // int a;
+    // cin>>a;
+    // char ch='a';
+
+    // cout<<"value of ch is: "<<ch<<endl;
+
+    // bool flag=true;
+    // cout<<"value of flag is "<<flag<<endl;
+
+
+    // long a=6;
+    // cout<<sizeof(a)<<endl;
+
+    // char ch=97;
+    // cout<<ch<<endl;
+
+    double d=5.7;
+    int x=(int)d+2;
+    cout<<"value of x is "<<x<<endl;
+
+      int ch='b';
+    cout<<ch<<endl;
+}
