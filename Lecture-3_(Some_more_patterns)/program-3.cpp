e1941005619152cfd59b0a7545af319729f8257d
Sachinkr <royalsachin1530@gmail.com>
2025-08-29 22:22:47 +0530

    Add files via upload

---
+++ Lecture-3_(Some_more_patterns)/program-3.cpp
@@ -0,0 +1,45 @@
+//  Solid Diamond 
+
+
+//    * 
+//   * *
+//  * * *
+// * * * *
+// * * * *
+//  * * *
+//   * *
+//    *
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+     for(int row=0;row<n;row++)
+      // space
+      {
+        for(int col=0; col<n-row-1; col++){
+            cout<<" ";
+        }
+        for (int col=0; col<row+1; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+         
+      }
+      // inverted code 
+    for(int row=0;  row<n; row++){
+        // spaces
+        for(int col=0; col<row; col++){
+            cout<<" ";
+        }
+        for(int col=0; col<n-row; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
