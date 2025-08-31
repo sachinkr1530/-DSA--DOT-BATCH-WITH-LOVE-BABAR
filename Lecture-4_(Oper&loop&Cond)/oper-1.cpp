88fc55e9cb6be78b8bb185bd7e55a21ce07fc9ff
Sachinkr <royalsachin1530@gmail.com>
2025-08-31 21:52:57 +0530

    Add files via upload

---
+++ Lecture-4_(Oper&loop&Cond)/oper-1.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+int   c=10;  // ye variable global varaible h 
+int main(){
+    // bool a=false;
+    // bool b=false;
+    // // cout<< (~a);
+    // // cout<<(a^b);
+    // cout<< (2 & 3)<<endl;
+
+    
+    //  left shift operator
+    // int a=12;
+    // a=a<<3;
+    // cout<<a<<endl;
+
+    //  Right shift operator 
+
+    // int a=12;
+    // a=a>>1;
+    // cout<<a<<endl;
+
+
+    //  pre increment
+    // int a=11;
+    // cout<<++a;
+
+    //  post increment 
+    
+    //    int a=11;
+    //  cout<<a++;  
+
+
+    // int a=5;
+    // cout<<(++a)*(++a);
+    
+
+    // break 
+
+    // int n=5;
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<"SAchin"<<endl;
+    //     break;
+    // }
+    // cout<<"Love "<<endl;
+
+
+    //  continue
+
+    // int a=5;
+    // for(int i=0;i<a;i++)
+    // {
+    //     continue;
+    //     cout<<"Sachin";
+    // }
+    // cout<<"kumar";
+
+// decalaration
+    int a;
+    //  initialisation 
+    int b=5;
+    //  updation
+
+    // b=10;  
+
+    if(true){ 
+        int b=15; // ye b local variable h  
+        cout<<b<<endl;
+    }
+    cout<<b<<endl;
+    cout<<c<<endl;
+}
---
+++ Lecture-4_(Oper&loop&Cond)/switch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int val;
+    cout<<"Enter the value => ";
+    cin>> val;
+
+    switch (val){
+        case 1: 
+        cout<<"Love";
+        break;
+        case 2: 
+        cout<<"Sachin";
+        break;
+        case 3: 
+        cout<<"Abhiraj";
+        break;
+        default:
+        cout<<"Moh maya";
+
+
+    }
+}
